6d4f9240fb2a959381a4ba3bb0db86d38a7f6963
Matthew Setter <matthew@matthewsetter.com>
2024-08-29 14:57:46 +1000

    Add a route to send a voice recording transcription via sms
    This route is the second part of the puzzle. It receives a text
    transcription of a voice recording from Twilio via a POST request and
    then sends that to the nominated phone number via SMS.

--- public/index.php
+++ public/index.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 use DI\Container;
 use Monolog\Handler\StreamHandler;
@@ -9,9 +10,9 @@ use Psr\Http\Message\ServerRequestInterface;
 use Psr\Log\LoggerInterface;
 use Slim\Factory\AppFactory;
 use Twilio\Rest\Client;
+use Twilio\TwiML\VoiceResponse;
 
 require __DIR__ . '/../vendor/autoload.php';
-
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../");
 $dotenv->load();
@@ -41,7 +42,6 @@ $container->set(
 );
 
 AppFactory::setContainer($container);
-
 $app = AppFactory::create();
 
 /**
@@ -67,20 +67,82 @@ function duringBusinessHours(
     );
 }
 
-$app->get('/', function (
+$app->post('/', function (
     ServerRequestInterface $request,
     ResponseInterface $response,
     array $args
 ): ResponseInterface {
-    $message = duringBusinessHours(
+    $isDuringBusinessHours = duringBusinessHours(
         new DateTimeImmutable('now'),
         $_ENV['WORK_WEEK_START'] ?? 'Monday',
         $_ENV['WORK_WEEK_END'] ?? 'Friday',
         $_ENV['WORK_HOUR_START'] ?? 8,
         $_ENV['WORK_HOUR_END'] ?? 18,
-    ) ? 'Inside business hours.'
-        : 'Currently outside of office hours.';
+    );
 
+    /** @var LoggerInterface $logger */
+    $logger = $this->get(LoggerInterface::class);
+    $logger->info(
+        $isDuringBusinessHours
+            ? "During business hours. Attempting to redirect."
+            : "Outside of business hours. Sending to voicemail."
+    );
+
+    $voiceResponse = new VoiceResponse();
+
+    if ($isDuringBusinessHours) {
+        $voiceResponse->dial($_ENV['MY_PHONE_NUMBER']);
+        $voiceResponse->say('Sorry, I was unable to redirect you. Goodbye.');
+    }
+
+    if (! $isDuringBusinessHours) {
+        $voiceResponse->record([
+            'finishOnKey' => '#',
+            'maxLength' => 300,
+            'timeout' => 10,
+            'transcribe' => 'true',
+            'transcribeCallback' => '/sms'
+        ]);
+    }
+
+    $response->withHeader('Content-Type', 'application/xml');
+    $response
+        ->getBody()
+        ->write($voiceResponse->asXML());
+
+    return $response;
+});
+
+/**
+ * This route receives a POST request with a voicemail recording transcription which is sent to the specified number.
+ */
+$app->post('/sms', function (
+    ServerRequestInterface $request,
+    ResponseInterface $response,
+    array $args
+): ResponseInterface {
+    /** @var LoggerInterface $logger */
+    $logger = $this->get(LoggerInterface::class);
+    $logger->info('Received SMS transcription request from Twilio', $request->getParsedBody());
+
+    /** @var Client $client */
+    $client = $this->get(Client::class);
+
+    $sms = $client
+        ->messages
+        ->create(
+            $_ENV['MY_PHONE_NUMBER'],
+            [
+                'body' => $request->getParsedBody()['transcription_text'],
+                'from' => (string)$_ENV['TWILIO_PHONE_NUMBER'],
+            ]
+        );
+
+    $message = match ($sms->status) {
+        'failed', 'canceled', 'undelivered' => 'Something went wrong sending the SMS with the voice recording transcript.',
+        default => 'The SMS with the voice recording transcript was sent successfully.'
+    };
+    $logger->info($message, [$sms]);
     $response->getBody()->write($message);
 
     return $response;
