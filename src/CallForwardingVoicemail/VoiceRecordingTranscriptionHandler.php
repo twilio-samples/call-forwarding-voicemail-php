42d66a27a25d3c7dfdb5d8a861eb66cf6aff389d
Matthew Setter <matthew@matthewsetter.com>
2024-10-28 15:56:34 +1000

    Add in class stubs for refactoring the core functionality

---
+++ src/CallForwardingVoicemail/VoiceRecordingTranscriptionHandler.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CallForwardingVoicemail\CallForwardingVoicemail;
+
+use PHPUnit\Framework\TestCase;
+
+class VoiceRecordingTranscriptionHandler extends TestCase
+{
+
+}
---
+++ test/CallForwardingVoicemail/VoiceRecordingTranscriptionHandlerTest.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace src\CallForwardingVoicemail;
+
+use PHPUnit\Framework\TestCase;
+
+class VoiceRecordingTranscriptionHandlerTest extends TestCase
+{
+
+}
